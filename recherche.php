b713e5e31e93e4544bb9cfc1d95a89daf3f7d7ed
Sébastien Gruet <sebastien-gruet@orange.fr>
2020-10-20 15:00:42 +0200

    Adding input field in recherche page

--- recherche.php
+++ recherche.php
@@ -1,3 +1,9 @@
+<?php
+require_once('connection.php');
+
+
+require_once('close;php');
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +13,19 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
 <body>
+    <main class="container">
+        <div class="row">
+            <section class="col-12">
+            <h1>Rechercher une personne</h1>
+            <form method="post">
+                <div class="form-group">
+                    <label for="name">Prenom: </label>
+                    <input type="text" name="name" id="name" class="form-control" />
+                </div>
+            </form>
+            </section>
+        </div>
+    </main>
     <a href="index.php">Retour</a>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
