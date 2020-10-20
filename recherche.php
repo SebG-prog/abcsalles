cd39402a3576e5cd0c56d2c5b08f827b04e30ecc
Sébastien Gruet <sebastien-gruet@orange.fr>
2020-10-20 15:12:12 +0200

    fetching person on recherche page

--- recherche.php
+++ recherche.php
@@ -1,8 +1,27 @@
 <?php
-require_once('connection.php');
+var_dump($_POST);
+if ($_POST) {
+    if (isset($_POST['label']) && !empty($_POST['label'])) {
+        
+        require_once('connection.php');
+        $label = strip_tags($_POST['label']);
 
+        $sql = 'SELECT * FROM `ref_prenoms` WHERE label=:label;';
+        $query = $db->prepare($sql);
 
-require_once('close.php');
+        $query->bindValue('label', $label, PDO::PARAM_STR);
+        $query->execute(); 
+
+        $person = $query->fetch();
+        var_dump($person);
+
+        require_once('close.php');
+        
+    } else {
+
+    }
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -19,15 +38,15 @@ require_once('close.php');
             <h1>Rechercher une personne</h1>
             <form method="post">
                 <div class="form-group">
-                    <label for="name">Prenom: </label>
-                    <input type="text" name="name" id="name" class="form-control" />
+                    <label for="label">Prenom: </label>
+                    <input type="text" name="label" id="label" class="form-control" />
                 </div>
                 <button type="submit" class="btn btn-primary">Rechercher</button>
             </form>
             </section>
         </div>
+        <a href="index.php">Retour</a>
     </main>
-    <a href="index.php">Retour</a>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
