d0a134b0b8f833178f139c419e8bf3ee9ca210c0
Sébastien Gruet <sebastien-gruet@orange.fr>
2020-10-20 16:39:23 +0200

    Research part finished. Beginning of treatment part

--- recherche.php
+++ recherche.php
@@ -1,27 +1,48 @@
 <?php
-var_dump($_POST);
 if ($_POST) {
     if (isset($_POST['label']) && !empty($_POST['label'])) {
+        define('PERSON',[
+            'type' => [
+                '0' => 'ambiguë',
+                '1' => 'masculin',
+                '2' => 'féminin'
+            ],
+            'genre' => [
+                '1' => 'masculin',
+                '2' => 'féminin'
+            ],
+            'highlight' => [
+                '0' => 'white',
+                '1' => 'lightblue',
+                '2' => 'lightpink'
+            ],
+        ]);
+
         try { 
             require_once('connection.php');
             $label = strip_tags($_POST['label']);
+            $label = ucfirst(strtolower($label));
     
-            $sql = 'SELECT * FROM `ref_prenoms` WHERE label LIKE :label'.'% ORDER BY label ASC;';
+            $sql = 'SELECT * FROM `ref_prenoms` WHERE label LIKE "' . $label . '%" ORDER BY label ASC;';
             $query = $db->prepare($sql);
     
             $query->bindValue('label', $label, PDO::PARAM_STR);
             $query->execute(); 
     
             $persons = $query->fetchAll();
-            var_dump($persons);
-    
+            if (!$persons) {
+                $errorMessage = 'No matching value';
+            } else {
+                $successMessage = count($persons) . ' results found.';
+            }
+
             require_once('close.php');
         } catch(PDOException $e) {
             echo 'Error: ' . $e->getMessage();
         }
         
     } else {
-
+        $errorMessage = 'Search field is empty';
     }
 }
 ?>
@@ -39,16 +60,52 @@ if ($_POST) {
         <div class="row">
             <section class="col-12">
             <h1>Rechercher une personne</h1>
+            <?php if(!empty($errorMessage)): ?>
+                <div class="alert alert-danger">
+                    <?= $errorMessage ?>
+                </div>
+                <?php $errorMessage="" ?>
+            <?php endif ?>
+            <?php if(!empty($successMessage)): ?>
+                <div class="alert alert-success">
+                    <?= $successMessage ?>
+                </div>
+                <?php $successMessage="" ?>
+            <?php endif ?>
             <form method="post">
                 <div class="form-group">
-                    <label for="label">Prenom: </label>
-                    <input type="text" name="label" id="label" class="form-control" />
+                    <label for="label">Prénom: </label>
+                    <input type="text" name="label" id="label" class="form-control" placeholder="Prénom" />
                 </div>
                 <button type="submit" class="btn btn-primary">Rechercher</button>
             </form>
+            <br />
+            <?php if (isset($persons) && !empty($persons)): ?>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Label</th>
+                            <th>Genre</th>
+                            <th>Type</th>
+                            <th>Origin</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach($persons as $person): ?>
+                            <tr style="background:<?= PERSON['highlight'][$person["type"]] ?>;">
+                                <td><?= $person['label'] ?></td>
+                                <td><?= PERSON['genre'][$person["genre"]] ?></td>
+                                <td><?= PERSON['type'][$person["type"]] ?></td>
+                                <td><?= $person['origin'] ?></td>
+                            </tr>
+                        <?php endforeach ?>   
+                    </tbody>
+                </table>
+            <?php endif ?>
             </section>
         </div>
-        <a href="index.php">Retour</a>
+        <br/>
+        <a href="index.php" class="btn btn-primary">Retour</a>
     </main>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
--- traitement.php
+++ traitement.php
@@ -1,3 +1,18 @@
+<?php
+var_dump($_POST);
+if ($_POST) {
+    if (isset($_POST['filename']) && !empty($_POST['filename'])) {
+        $file = __DIR__ . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . $_POST['filename'] . '.csv';
+        $ressource = fopen($file, 'r');
+        while($line = fgets($ressource)) {
+            var_dump($line);
+        }
+        fclose($ressource);
+    }
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,7 +22,22 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
 <body>
-    <a href="index.php">Retour</a>
+    <main class="container">
+        <div class="row">
+            <section class="col-12">
+            <form method="post">
+                <label for="filename">Choisir un fichier: </label>
+                <select name="filename" id="filename">
+                    <optgroup label="Select a file">
+                        <option value="a-traiter">a-traiter</option>
+                    </optgroup>
+                </select>
+                <button type="submit" class="btn btn-primary">Valider</button>
+            </form>
+            </section>
+        </div>
+        <a href="index.php" class="btn btn-primary">Retour</a>
+    </main>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
